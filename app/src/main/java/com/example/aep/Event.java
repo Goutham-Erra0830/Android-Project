8eb42c8051f310eff747b314413e6c0685bdc509
tarunyeluri <tarunyelurica@gmail.com>
2023-10-30 00:14:51 -0400

    Coach and student Activity code changes

---
+++ app/src/main/java/com/example/aep/Event.java
@@ -0,0 +1,17 @@
+package com.example.aep;
+
+import java.util.Date;
+
+public class Event {
+    private String title;
+    private String description;
+    private Date date;
+
+    public Event(String title, String description, Date date) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    // Getters and setters for the properties
+}
---
+++ app/src/main/java/com/example/aep/ui/home/StudentHomeFragment.java
@@ -0,0 +1,66 @@
+package com.example.aep.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.aep.Event;
+import com.example.aep.R;
+import com.example.aep.databinding.FragmentHomeBinding;
+import com.example.aep.databinding.FragmentStudenthomeBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentHomeFragment extends Fragment {
+
+    private @NonNull FragmentStudenthomeBinding binding;
+    private ImageView SportsRegistration;
+
+    private ViewPager eventViewPager;
+    private List<Event> events = new ArrayList<>();
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentStudenthomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        // Initialize imageView from the layout
+        SportsRegistration = binding.sportsRegistration;
+
+
+
+        // Set a click listener for imageView1
+        SportsRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Use the Navigation component to navigate to the new destination
+                Navigation.findNavController(v).navigate(R.id.action_homeStudentFragment_to_SportsRegistrationActivity);
+            }
+        });
+
+        return root;
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
