5a547dc2943aad601b83f4c98335f335b1a90f56
gouthamerram <erra0830@mylaurier.ca>
2023-11-21 15:15:19 -0500

    Created New Player Activity

--- app/src/main/java/com/example/aep/PlayerActivity.java
+++ app/src/main/java/com/example/aep/PlayerActivity.java
@@ -10,5 +10,6 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
     }
 }
