066c6314075fc3b9059fb4ecddef5b4965f14eb5
tarunyeluri <tarunyelurica@gmail.com>
2023-11-25 00:24:13 -0500

    Design for Singup and Login and Register activity

--- app/src/main/java/com/example/aep/PlayerActivity.java
+++ app/src/main/java/com/example/aep/PlayerActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PlayerActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
 
     }
 
@@ -24,6 +30,12 @@ public class PlayerActivity extends AppCompatActivity {
     public void SportsNews(View view)
     {
         Intent intent = new Intent(PlayerActivity.this, SportsNewsActivity.class);
+        startActivity(intent);
+    }
+
+    public void Profile(View view)
+    {
+        Intent intent = new Intent(PlayerActivity.this, ProfileActivity.class);
         startActivity(intent);
     }
 }
